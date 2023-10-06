3b3fc8346a5d0f5c89e1e3354b6d1444c30e33f9
pabloramon4ta <144847589+pabloramon4ta@users.noreply.github.com>
2023-10-06 09:46:20 +0200

    Add files via upload

---
+++ ejercicio24.py
@@ -0,0 +1,9 @@
+#24. Corrige los errores del siguiente código y comprueba que se ejecuta correctamente 
+var1=float(input("Introduce el peso: "))
+var2=float(input("Introduce la altura: "))
+var_imc=var1 / var2**2
+print(f"Si pesas {var1} kilos y mides {var2}, tu IMC es:",f"{var_imc:.2f}")
+if var_imc>25:
+     print("Hay sobrepeso")
+else:
+ print("Estás dentro de los parámetros normales")
